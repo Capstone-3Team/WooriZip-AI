1647f319d0af9225efaaa83dd8e774cd1bd3762f
yuzumelie <162935268+yuzumelie@users.noreply.github.com>
2025-12-04 17:16:43 +0900

    Update thumb_stt.py

--- models/thumb_stt.py
+++ models/thumb_stt.py
@@ -21,11 +21,11 @@ import mediapipe as mp
 # ============================================================
 mp_face_mesh = mp.solutions.face_mesh
 mesh_detector = mp_face_mesh.FaceMesh(
+    static_image_mode=True,  # 🔥 GPU 그래프 완전 차단 + CPU 전용
     max_num_faces=1,
     refine_landmarks=True,
     min_detection_confidence=0.5,
-    min_tracking_confidence=0.5,
-    static_image_mode=False  # 🔥 CPU-only fallback 강제
+    min_tracking_confidence=0.5
 )
 
 
