043ed36340ad631e8973ebf37bfe34e289e215e0
yuzumelie <162935268+yuzumelie@users.noreply.github.com>
2025-12-05 11:26:39 +0900

    Update face_arrange.py

--- models/face_arrange.py
+++ models/face_arrange.py
@@ -1,6 +1,8 @@
 import os
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-os.environ["MEDIAPIPE_DISABLE_GPU"] = "1"
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"     # GPU 비활성화
+os.environ["MEDIAPIPE_DISABLE_GPU"] = "1"     # Mediapipe GPU 금지
+
 import cv2
 import mediapipe as mp
 import numpy as np
